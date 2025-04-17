1044a6614d8eb316327899a2847f0cf04f0853d5
HARIKA <2200030238@kluniversity.in>
2025-04-17 10:06:35 +0530

    First commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: true, // Expose to network
     port: 5173,
-  },
+    allowedHosts: ['reactcrudcooknest-production.up.railway.app'],
+  },
 });
